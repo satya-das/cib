6437d12815c5a5d6cb31506fdf78a791ef7322d9
satya <beingsatyadas@gmail.com>
2017-10-18 18:44:11 +0200

    Minor change.

--- cibfunction.h
+++ cibfunction.h
@@ -15,20 +15,20 @@ struct CibParams;
  * Base class to add common attributes to function-like objects.
 
  * It can be very easy to go wrong when dealing with
- * CppConstructor, CppDestructor, and CppFunction; so,
- * access to them is blocked by having protected inheritance.
+ * CppConstructor, CppDestructor, and CppFunction in uniform way;
+ * so, access to them is blocked by having protected inheritance.
  * CibFunctionHelper should be used instead of directly accessing
  * CppConstructor, CppDestructor, and CppFunction objects.
+ * Using CibFunctionHelper it is possible to use these three
+ * C++ constructs uniformly.
  */
 template <typename _Base>
 struct CppFunctionLikeMixin : protected _Base
 {
-  std::string procNameSfx_;               // Suffix to be used for proc name. It is needed to make overloaded methods have different proc name.
-
+protected:
   using _Base::_Base;
 
-protected:
-  CppFunctionLikeMixin() {}
+  std::string procNameSfx_; // Suffix to be used for proc name. It is needed to make overloaded methods have different proc name.
 
   friend class CibFunctionHelper;
 };
