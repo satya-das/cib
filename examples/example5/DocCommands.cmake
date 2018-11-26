5e5fca06b8cb31c5e6b2aa93a8378f88f10e2b2a
Satya Das <SatyaRanjan.Das@tomtom.com>
2018-11-26 19:11:59 +0100

    Removed timestamp from diff files.

--- examples/example5/DocCommands.cmake
+++ examples/example5/DocCommands.cmake
@@ -3,7 +3,7 @@ set(${example}DiffFilesForReadMe ${example_ROOT}/header-diff.diff)
 add_custom_command(
     OUTPUT ${${example}DiffFilesForReadMe}
     COMMAND mkdir -p ${example_ROOT}/../readme-assets/${example}
-    COMMAND diff -Bw -u1000 pub/example.h exp/example.h >${example_ROOT}/../readme-assets/${example}/header-diff.diff || echo $$\? >/dev/null
+    COMMAND diff -Bw -u1000 --label pub/example.h --label exp/example.h pub/example.h exp/example.h >${example_ROOT}/../readme-assets/${example}/header-diff.diff || echo $$\? >/dev/null
     DEPENDS ${example_ROOT}/pub/example.h ${example_ROOT}/exp/example.h
     WORKING_DIRECTORY ${example_ROOT}
 )
