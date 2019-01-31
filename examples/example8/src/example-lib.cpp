a9e17a329acf0272ca4a4dfafae4f83a225f7a32
Satya <beingsatyadas@gmail.com>
2019-01-31 20:45:48 +0100

    Fixing Windows compilation error.

--- examples/example8/src/example-lib.cpp
+++ examples/example8/src/example-lib.cpp
@@ -10,17 +10,23 @@ RValueExample& RValueExample::operator=(RValueExample&& rhs)
 {
   mValue     = rhs.mValue;
   rhs.mValue = 0;
+
+  return *this;
 }
 
 RValueExample& RValueExample::operator=(const RValueExample& rhs)
 {
   mValue = rhs.mValue;
+
+  return *this;
 }
 
 std::int32_t RValueExample::Set(RValueExample&& x)
 {
   mValue   = x.mValue;
   x.mValue = 0;
+
+  return 0;
 }
 
 std::int32_t RValueExample::GetValue() const
