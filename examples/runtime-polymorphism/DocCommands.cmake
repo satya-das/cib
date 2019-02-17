bde5a03d8df06ea1a253e505c9ebdb14ff9e8316
Satya Das <SatyaRanjan.Das@tomtom.com>
2019-02-17 03:30:46 +0100

    Modularization of ReadMe.

--- examples/runtime-polymorphism/DocCommands.cmake
+++ examples/runtime-polymorphism/DocCommands.cmake
@@ -8,12 +8,12 @@ set(${example}DiffFilesForReadMe
 
 add_custom_command(
     OUTPUT ${${example}DiffFilesForReadMe}
-    COMMAND mkdir -p ${example_ROOT}/../readme-assets/${example}
-    COMMAND diff -Bw -U1000 --label pub/example.h --label exp/example.h pub/example.h exp/example.h >../readme-assets/${example}/header-diff.diff || echo "Ignore failure" >/dev/null
-    COMMAND diff -Bw -u --label ../simple-class/cib/__zz_cib_Example-ids.h ../simple-class/cib/__zz_cib_Example-ids.h --label cib/__zz_cib_Example-ids.h cib/__zz_cib_Example-ids.h >../readme-assets/${example}/ids-diff.diff || echo "Ignore failure" >/dev/null
-    COMMAND diff -Bw -u --label ../simple-class/cib/example.h.cpp --label cib/example.h.cpp ../simple-class/cib/example.h.cpp cib/example.h.cpp >../readme-assets/${example}/lib-glue-diff.diff || echo "Ignore failure" >/dev/null
-    COMMAND diff -Bw -U1000 --label ../simple-class/exp/example.h --label exp/example.h ../simple-class/exp/example.h exp/example.h >../readme-assets/${example}/proxy-diff.diff || echo "Ignore failure" >/dev/null
-    COMMAND diff -Bw -u --label ../simple-class/exp/__zz_cib_internal/example-impl.h --label exp/__zz_cib_internal/example-impl.h ../simple-class/exp/__zz_cib_internal/example-impl.h exp/__zz_cib_internal/example-impl.h >../readme-assets/${example}/impl-diff.diff || echo "Ignore failure" >/dev/null
+    COMMAND mkdir -p ${example_ROOT}/readme-assets/
+    COMMAND diff -Bw -U1000 --label pub/example.h --label exp/example.h pub/example.h exp/example.h >readme-assets/header-diff.diff || echo "Ignore failure" >/dev/null
+    COMMAND diff -Bw -u --label ../simple-class/cib/__zz_cib_Example-ids.h ../simple-class/cib/__zz_cib_Example-ids.h --label cib/__zz_cib_Example-ids.h cib/__zz_cib_Example-ids.h >readme-assets/ids-diff.diff || echo "Ignore failure" >/dev/null
+    COMMAND diff -Bw -u --label ../simple-class/cib/example.h.cpp --label cib/example.h.cpp ../simple-class/cib/example.h.cpp cib/example.h.cpp >readme-assets/lib-glue-diff.diff || echo "Ignore failure" >/dev/null
+    COMMAND diff -Bw -U1000 --label ../simple-class/exp/example.h --label exp/example.h ../simple-class/exp/example.h exp/example.h >readme-assets/proxy-diff.diff || echo "Ignore failure" >/dev/null
+    COMMAND diff -Bw -u --label ../simple-class/exp/__zz_cib_internal/example-impl.h --label exp/__zz_cib_internal/example-impl.h ../simple-class/exp/__zz_cib_internal/example-impl.h exp/__zz_cib_internal/example-impl.h >readme-assets/impl-diff.diff || echo "Ignore failure" >/dev/null
     DEPENDS ${example_ROOT}/pub/example.h ${example_ROOT}/exp/example.h
     WORKING_DIRECTORY ${example_ROOT}
 )
--- examples/virtual-function-and-abi-stability/DocCommands.cmake
+++ examples/virtual-function-and-abi-stability/DocCommands.cmake
@@ -2,9 +2,9 @@ set(${example}DiffFilesForReadMe ${example_ROOT}/header-diff.diff ${example_ROOT
 
 add_custom_command(
     OUTPUT ${${example}DiffFilesForReadMe}
-    COMMAND mkdir -p ${example_ROOT}/../readme-assets/${example}
-    COMMAND diff -Bw -u1000 --label ${PREV_TEST_NAME}/pub/example.h --label ${NEW_TEST_NAME}/pub/example.h ${example_ROOT}/../${PREV_TEST_NAME}/pub/example.h ${example_ROOT}/pub/example.h >${example_ROOT}/../readme-assets/${example}/header-diff.diff || echo "Ignore failure" >/dev/null
-    COMMAND diff -Bw -u1000 --label ${PREV_TEST_NAME}/src/example-client.cpp --label ${NEW_TEST_NAME}/src/example-client.cpp ${example_ROOT}/../${PREV_TEST_NAME}/src/example-client.cpp ${example_ROOT}/src/example-client.cpp >${example_ROOT}/../readme-assets/${example}/client-diff.diff || echo "Ignore failure" >/dev/null
+    COMMAND mkdir -p ${example_ROOT}/readme-assets/
+    COMMAND diff -Bw -u1000 --label ${PREV_TEST_NAME}/pub/example.h --label ${NEW_TEST_NAME}/pub/example.h ${example_ROOT}/../${PREV_TEST_NAME}/pub/example.h ${example_ROOT}/pub/example.h >${example_ROOT}/readme-assets/header-diff.diff || echo "Ignore failure" >/dev/null
+    COMMAND diff -Bw -u1000 --label ${PREV_TEST_NAME}/src/example-client.cpp --label ${NEW_TEST_NAME}/src/example-client.cpp ${example_ROOT}/../${PREV_TEST_NAME}/src/example-client.cpp ${example_ROOT}/src/example-client.cpp >${example_ROOT}/readme-assets/client-diff.diff || echo "Ignore failure" >/dev/null
     DEPENDS ${example_ROOT}/pub/example.h ${example_ROOT}/../${PREV_TEST_NAME}/pub/example.h ${example_ROOT}/src/example-client.cpp ${example_ROOT}/../${PREV_TEST_NAME}/src/example-client.cpp
     WORKING_DIRECTORY ${example_ROOT}
 )
