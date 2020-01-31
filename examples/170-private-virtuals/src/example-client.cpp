2adfdc536faf95ad6447248f9e5000fba4fe1852
Satya Das <SatyaRanjan.Das@tomtom.com>
2020-01-31 19:59:57 +0100

    minor

--- examples/170-private-virtuals/src/example-client.cpp
+++ examples/170-private-virtuals/src/example-client.cpp
@@ -2,6 +2,36 @@
 
 #include <catch/catch.hpp>
 
+class OverriddenOnlyPureVirtualIF : public IF
+{
+private:
+  int PrivatePureVirtual() override { return 1000; }
+
+protected:
+  int ProtectedPureVirtual() override
+  {
+    return ProtectedNonVirtual();
+  }
+
+public:
+  int PublicPureVirtual() override  { return 3000; }
+};
+
+/*
+This test is crashing because of infinite recursion.
+As of now only private pure virtual is supported.
+If private virtual is not overridden by client then it causes infinite recursion.
+
+TEST_CASE("Virtual methods of all access type of parent should be exported.")
+{
+  OverriddenOnlyPureVirtualIF ifObj;
+
+  IF* pIF = &ifObj;
+
+  CHECK(pIF->PublicNonVirtual() == 3000 + 500 + 400 + 400 + 1000 + 100 + 200);
+}
+*/
+
 class OverriddenIF : public IF
 {
 private:
