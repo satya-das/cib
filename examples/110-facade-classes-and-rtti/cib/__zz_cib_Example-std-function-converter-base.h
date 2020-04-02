15e9c5b253e76c470840bfe30b2956895c830d1f
Satya Das <SatyaRanjan.Das@tomtom.com>
2020-04-02 04:01:49 +0200

    improvements in std::function handling needed for skia

--- examples/110-facade-classes-and-rtti/cib/__zz_cib_Example-std-function-converter-base.h
+++ examples/110-facade-classes-and-rtti/cib/__zz_cib_Example-std-function-converter-base.h
@@ -55,61 +55,127 @@ inline void __zz_cib_decl AbiFunctor_destruct(const __zz_cib_AbiFunctor<R, Args.
 }
 
 template <typename R, typename... Args>
-inline __zz_cib_AbiFunctor<R, Args...> __zz_cib_decl AbiFunctor_copy(const __zz_cib_AbiFunctor<R, Args...>* abiFunctor)
+__zz_cib_AbiFunctor<R, Args...> __zz_cib_decl AbiFunctor_copy(const __zz_cib_AbiFunctor<R, Args...>* abiFunctor);
+
+/**
+ * Helps in invoking function pointer contained in __zz_cib_AbiFunctor
+ *
+ * @note It is required because we need to specialize for the case when "R = void".
+ */
+template <typename R, typename... Args>
+struct AbiFunctorCallHelper
 {
-  return toAbiFunctor(new std::function<R(Args...)>(*AbiFunctor_getStdFunc<R, Args...>(abiFunctor)),
-                      AbiFunctor_destruct);
-}
+  const __zz_cib_AbiFunctor<R, Args...>* const mAbiFunctor;
+
+  AbiFunctorCallHelper(const __zz_cib_AbiFunctor<R, Args...>* abiFunctor)
+    : mAbiFunctor(abiFunctor)
+  {
+  }
+
+  __zz_cib_AbiType_t<R> operator()(__zz_cib_AbiType_t<Args>... args) const
+  {
+    auto& origStdFunc = *AbiFunctor_getStdFunc<R, Args...>(mAbiFunctor);
+    return __zz_cib_ToAbiType<R>(origStdFunc(__zz_cib_FromAbiType<Args>(args)...));
+  }
+};
+
+template <typename... Args>
+struct AbiFunctorCallHelper<void, Args...>
+{
+  const __zz_cib_AbiFunctor<void, Args...>* const mAbiFunctor;
+
+  AbiFunctorCallHelper(const __zz_cib_AbiFunctor<void, Args...>* abiFunctor)
+    : mAbiFunctor(abiFunctor)
+  {
+  }
+
+  void operator()(__zz_cib_AbiType_t<Args>... args) const
+  {
+    auto& origStdFunc = *AbiFunctor_getStdFunc<void, Args...>(mAbiFunctor);
+    return origStdFunc(__zz_cib_FromAbiType<Args>(args)...);
+  }
+};
 
 template <typename R, typename... Args>
 inline __zz_cib_AbiType_t<R> __zz_cib_decl AbiFunctor_call(const __zz_cib_AbiFunctor<R, Args...>* abiFunctor,
                                                            __zz_cib_AbiType_t<Args>... args)
 {
-  auto& origStdFunc = *AbiFunctor_getStdFunc<R, Args...>(abiFunctor);
-  return __zz_cib_ToAbiType<R>(origStdFunc(__zz_cib_FromAbiType<Args>(args)...));
+  AbiFunctorCallHelper call(abiFunctor);
+  return call(args...);
 };
 
 /**
  * A wrapper on __zz_cib_AbiFunctor to destroy the underlying object smartly.
  */
 template <typename R, typename... Args>
-class __zz_cib_SmartFunctor
+class __zz_cib_SmartFunctorBase
 {
+protected:
   using AbiFunctor = __zz_cib_AbiFunctor<R, Args...>;
 
   AbiFunctor mAbiFunctor;
 
+private:
   void disableDtor()
   {
     mAbiFunctor.dtor = nullptr;
   }
 
 public:
-  __zz_cib_SmartFunctor(AbiFunctor abiFunctor)
+  __zz_cib_SmartFunctorBase(AbiFunctor abiFunctor)
     : mAbiFunctor(abiFunctor)
   {
   }
 
-  __zz_cib_SmartFunctor(__zz_cib_SmartFunctor&& rhs)
+  __zz_cib_SmartFunctorBase(__zz_cib_SmartFunctorBase&& rhs)
     : mAbiFunctor(rhs.mAbiFunctor)
   {
     rhs.disableDtor();
   }
 
-  __zz_cib_SmartFunctor(const __zz_cib_SmartFunctor& rhs)
+  __zz_cib_SmartFunctorBase(const __zz_cib_SmartFunctorBase& rhs)
     : mAbiFunctor(rhs.mAbiFunctor.copy(&rhs.mAbiFunctor))
   {
   }
 
-  ~__zz_cib_SmartFunctor()
+  ~__zz_cib_SmartFunctorBase()
   {
     if (mAbiFunctor.dtor)
       mAbiFunctor.dtor(&mAbiFunctor);
   }
+};
+
+template <typename R, typename... Args>
+class __zz_cib_SmartFunctor : public __zz_cib_SmartFunctorBase<R, Args...>
+{
+  const typename __zz_cib_SmartFunctorBase<R, Args...>::AbiFunctor* abiFunctor() const
+  {
+    return &(this->__zz_cib_SmartFunctorBase<R, Args...>::mAbiFunctor);
+  }
+
+public:
+  using __zz_cib_SmartFunctorBase<R, Args...>::__zz_cib_SmartFunctorBase;
 
   R operator()(Args... args) const
   {
-    return __zz_cib_FromAbiType<R>(mAbiFunctor.proc(&mAbiFunctor, __zz_cib_ToAbiType<Args>(args)...));
+    return __zz_cib_FromAbiType<R>(abiFunctor()->proc(abiFunctor(), __zz_cib_ToAbiType<Args>(args)...));
+  }
+};
+
+template <typename... Args>
+class __zz_cib_SmartFunctor<void, Args...> : public __zz_cib_SmartFunctorBase<void, Args...>
+{
+  const typename __zz_cib_SmartFunctorBase<void, Args...>::AbiFunctor* abiFunctor() const
+  {
+    return &(this->__zz_cib_SmartFunctorBase<void, Args...>::mAbiFunctor);
+  }
+
+public:
+  using __zz_cib_SmartFunctorBase<void, Args...>::__zz_cib_SmartFunctorBase;
+
+  void operator()(Args... args) const
+  {
+    return abiFunctor()->proc(abiFunctor(), __zz_cib_ToAbiType<Args>(args)...);
   }
 };
 
@@ -155,6 +221,13 @@ inline std::function<R(Args...)> fromAbiFunctor(const __zz_cib_AbiFunctor<R, Arg
   if (abiFunctor.proc == nullptr)
     return nullptr;
   return __zz_cib_SmartFunctor<R, Args...>(abiFunctor);
+}
+
+template <typename R, typename... Args>
+inline __zz_cib_AbiFunctor<R, Args...> __zz_cib_decl AbiFunctor_copy(const __zz_cib_AbiFunctor<R, Args...>* abiFunctor)
+{
+  return toAbiFunctor(new std::function<R(Args...)>(*AbiFunctor_getStdFunc<R, Args...>(abiFunctor)),
+                      AbiFunctor_destruct);
 }
 
 } // namespace __zz_cib_
