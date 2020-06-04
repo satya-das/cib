db067bfa5653e07ca6f5175bd5be9b9281db2f40
Satya Das <SatyaRanjan.Das@tomtom.com>
2020-06-04 20:28:23 +0200

    fixed dependencies

--- examples/epilogue-common-bkwd-compatibility.cmake
+++ examples/epilogue-common-bkwd-compatibility.cmake
@@ -3,15 +3,15 @@ set(NEW_LIBNAME  "${LIBRARY_PREFIX}${example}_lib${LIBRARY_SUFFIX}")
 set(CLIENT       "${PREV_TEST_NAME}${CLIENTSUFFIX}")
 set(TEST_NAME    "${example}-new-lib-with-old-client")
 
-add_custom_target(
-    ${TEST_NAME} ALL
-        ${CMAKE_COMMAND} -E make_directory ${TEST_NAME}
-        COMMAND ${CMAKE_COMMAND} -E copy
-            ${EXAMPLES_BIN_DIR}/${CLIENT} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${CLIENT}
-        COMMAND ${CMAKE_COMMAND} -E copy
-            ${EXAMPLES_BIN_DIR}/${NEW_LIBNAME} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${PREV_LIBNAME}
-        DEPENDS ${example}_lib ${PREV_TEST_NAME}
-        WORKING_DIRECTORY ${EXAMPLES_BIN_DIR}
+add_custom_command(
+    OUTPUT ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${CLIENT} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${PREV_LIBNAME}
+    ${CMAKE_COMMAND} -E make_directory ${TEST_NAME}
+    COMMAND ${CMAKE_COMMAND} -E copy
+        ${EXAMPLES_BIN_DIR}/${CLIENT} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${CLIENT}
+    COMMAND ${CMAKE_COMMAND} -E copy
+        ${EXAMPLES_BIN_DIR}/${NEW_LIBNAME} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${PREV_LIBNAME}
+    DEPENDS ${example} ${PREV_TEST_NAME}
+    WORKING_DIRECTORY ${EXAMPLES_BIN_DIR}
 )
 
 add_test(NAME ${TEST_NAME}
--- examples/epilogue-common-forwd-compatibility.cmake
+++ examples/epilogue-common-forwd-compatibility.cmake
@@ -3,15 +3,15 @@ set(NEW_LIBNAME  "${LIBRARY_PREFIX}${example}_lib${LIBRARY_SUFFIX}")
 set(CLIENT       "${example}${CLIENTSUFFIX}")
 set(TEST_NAME    "${example}-new-client-with-old-lib")
 
-add_custom_target(
-    ${TEST_NAME} ALL
-        ${CMAKE_COMMAND} -E make_directory ${TEST_NAME}
-        COMMAND ${CMAKE_COMMAND} -E copy
-            ${EXAMPLES_BIN_DIR}/${CLIENT} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${CLIENT}
-        COMMAND ${CMAKE_COMMAND} -E copy
-            ${EXAMPLES_BIN_DIR}/${PREV_LIBNAME} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${NEW_LIBNAME}
-        DEPENDS ${example} ${PREV_TEST_NAME}
-        WORKING_DIRECTORY ${EXAMPLES_BIN_DIR}
+add_custom_command(
+    OUTPUT ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${CLIENT} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${NEW_LIBNAME}
+    ${CMAKE_COMMAND} -E make_directory ${TEST_NAME}
+    COMMAND ${CMAKE_COMMAND} -E copy
+        ${EXAMPLES_BIN_DIR}/${CLIENT} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${CLIENT}
+    COMMAND ${CMAKE_COMMAND} -E copy
+        ${EXAMPLES_BIN_DIR}/${PREV_LIBNAME} ${EXAMPLES_BIN_DIR}/${TEST_NAME}/${NEW_LIBNAME}
+    DEPENDS ${example} ${PREV_TEST_NAME}
+    WORKING_DIRECTORY ${EXAMPLES_BIN_DIR}
 )
 
 add_test(NAME ${TEST_NAME}
