8cbde902770bcc01323f7eb18a6e8408f9143682
Satya Das <beingsatyadas@gmail.com>
2021-12-04 19:06:40 +0100

    changes as per changes in cib

--- case-studies/PoDoFo/podofo/cibification/cib.sh
+++ case-studies/PoDoFo/podofo/cibification/cib.sh
@@ -1,11 +1,14 @@
-~/github/cib/builds/cib -m CibPoDoFo -b cib/.. -i src/ -o ../exp/                         \
--A PODOFO_DEPRECATED                                                                      \
--A PODOFO_API                                                                             \
--A PODOFO_DOC_API                                                                         \
--M PODOFO_RAISE_LOGIC_IF                                                                  \
--I PODOFO_LOCAL                                                                           \
--I PDF_SIZE_FORMAT                                                                        
-
-#git checkout exp/podofo/base/PdfEncrypt.cpp
-#git checkout exp/__zz_cib_internal/__zz_cib_Class264.h
-
+../../../../build/cib                                       \
+  --module CibPoDoFo                                        \
+  --bind-folder ../cib/                                     \
+  --input-folder src/                                       \
+  --output-folder ../exp/                                   \
+  --cib-ids-file ../cib/__zz_cib_CibPoDoFo-ids.h            \
+  --apidecor PODOFO_DEPRECATED                              \
+  --apidecor PODOFO_API                                     \
+  --apidecor PODOFO_DOC_API                                 \
+  --apidecor PODOFO_EXCEPTION_API_DOXYGEN                   \
+  --apidecor PODOFO_NOTHROW                                 \
+  --macro    PODOFO_RAISE_LOGIC_IF                          \
+  --ignorable-macro PODOFO_LOCAL                            \
+  --ignorable-macro PDF_SIZE_FORMAT
